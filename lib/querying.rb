6edc44c006e8c349216cb9db9dfa93efa484be16
Matt Wright <mttwright@gmail.com>
2019-12-07 20:25:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT "
+  "SELECT characters.species, COUNT(characters.species)
+  FROM characters
+  GROUP BY characters.species;"
 end
 
 def select_name_and_series_subgenres_of_authors
