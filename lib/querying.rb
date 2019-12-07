dfb180ecfff496de55651197d856d69c7bf864b0
Matt Wright <mttwright@gmail.com>
2019-12-07 20:38:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,9 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title, COUNT(characters.species)
-  FROM series"
+  FROM series
+  JOIN characters
+  ON series.author_id = characters."
 end
 
 def select_character_names_and_number_of_books_they_are_in
