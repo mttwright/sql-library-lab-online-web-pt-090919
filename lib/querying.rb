d43a4fd87504a59589703362022ef19725aeb128
Matt Wright <mttwright@gmail.com>
2019-12-07 20:44:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,8 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON series.author_id = characters.author_id
   WHERE characters.species = 'human'
-  GROUP BY series.title;"
+  GROUP BY series.title
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
