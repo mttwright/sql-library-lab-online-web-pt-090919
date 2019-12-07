a28b2ab91deced97caeca770a71703e59686f6eb
Matt Wright <mttwright@gmail.com>
2019-12-07 20:31:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
   JOIN series
-  ON authors"
+  ON authors.id = series."
 end
 
 def select_series_title_with_most_human_characters
